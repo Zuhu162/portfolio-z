b20cd9a32313b89e9b6217661f2ad61a62927a69
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2024-09-27 20:42:19 +0600

    Metatags updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,32 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Portfolio | Zuhayer",
+  title: "Portfolio | Zuhayer Siddique",
   keywords:
-    "Zuhayer, Zuhayer Siddique, Zuhayer Adnan Siddique, Software Engineer, MERN Stack Developer, React Developer, JavaScript, TypeScript, NextJS, University of Technology Malaysia, Software Engineering Student, Project Portfolio",
+    "Zuhayer, Zuhayer Bangladesh, Zuhayer Malaysia, Zuhayer UTM, Zuhayer Universiti Teknologi Malaysia, Zuhayer Siddique, Zuhayer Adnan Siddique, Software Engineer Bangladesh, Software Engineer, MERN Stack Developer, React Developer, JavaScript, TypeScript, NextJS, University of Technology Malaysia, Software Engineering Student, Project Portfolio",
   description:
-    "Explore the portfolio of Zuhayer a software engineer specializing in MERN Stack development. View projects, skills, and download my resume",
+    "Software engineer specializing in MERN Stack development. Explore my projects, skills, and resume.",
+  icons: {
+    icon: [
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        url: "/Favicons/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/Favicons/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/Favicons/favicon-16x16.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
