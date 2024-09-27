70cdb82b9aa7e331169ef1da047f6ef8825f0010
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2024-09-28 02:20:41 +0600

    Basepath check

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ogImage from "./opengraph-image.jpg";
+import ogImage from "./opengraph-image.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
